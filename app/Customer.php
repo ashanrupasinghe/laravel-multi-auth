4560b04ac209fed3064c69f8b40b47ae7856ec0e
ashan <ashanrupasinghe11@gmail.com>
2017-02-04 23:55:49 +0530

    customer pw reset sucess

--- app/Customer.php
+++ app/Customer.php
@@ -3,10 +3,22 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use App\Notifications\UserCustomerResetPasswordNotification;
 
 class Customer extends User
 {
     //
     protected $table="customers";
+    
+    /**
+     * Send the password reset notification.
+     *
+     * @param  string  $token
+     * @return void
+     */
+    public function sendPasswordResetNotification($token)
+    {
+    	$this->notify(new UserCustomerResetPasswordNotification($token));
+    }
     
 }
--- app/Http/Controllers/CustomerAuth/ResetPasswordController.php
+++ app/Http/Controllers/CustomerAuth/ResetPasswordController.php
@@ -4,6 +4,9 @@ namespace App\Http\Controllers\CustomerAuth;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Foundation\Auth\ResetsPasswords;
+use Auth;
+use Illuminate\Support\Facades\Password;
+use Illuminate\Http\Request;
 
 class ResetPasswordController extends Controller
 {
@@ -25,7 +28,7 @@ class ResetPasswordController extends Controller
      *
      * @var string
      */
-    protected $redirectTo = '/home';
+    protected $redirectTo = '/customer_home';
 
     /**
      * Create a new controller instance.
@@ -35,5 +38,43 @@ class ResetPasswordController extends Controller
     public function __construct()
     {
         $this->middleware('guest');
+    }
+    
+    
+    
+    /**
+     * Display the password reset view for the given token.
+     *
+     * If no token is present, display the link request form.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  string|null  $token
+     * @return \Illuminate\Contracts\View\Factory|\Illuminate\View\View
+     */
+    public function showResetForm(Request $request, $token = null)
+    {
+    	return view('customer-auth.passwords.reset')->with(
+    			['token' => $token, 'email' => $request->email]
+    	);
+    }
+    
+    /**
+     * Get the broker to be used during password reset.
+     *
+     * @return \Illuminate\Contracts\Auth\PasswordBroker
+     */
+    public function broker()
+    {
+    	return Password::broker('customers');
+    }
+    
+    /**
+     * Get the guard to be used during password reset.
+     *
+     * @return \Illuminate\Contracts\Auth\StatefulGuard
+     */
+    protected function guard()
+    {
+    	return Auth::guard('customer');
     }
 }
